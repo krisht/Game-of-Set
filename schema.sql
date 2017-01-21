19554e3bfa77240f2301d94d2fe7b37d920ee888
krisht <krisht@cooper.edu>
2017-01-20 21:44:27 -0500

    Added more tables

--- schema.sql
+++ schema.sql
@@ -7,11 +7,25 @@ GRANT ALL PRIVILEGES ON ReadySetGo.* to admin@localhost IDENTIFIED BY 'admin';
 USE ReadySetGo; 
 
 CREATE TABLE 'Users'(
-	uname VARCHAR(128) NOT NULL,
+	uid VARCHAR(128) NOT NULL,
 	name VARCHAR(128), 
 	password VARCHAR(64),
 	PRIMARY KEY(username)
 ); 
+
+CREATE TABLE 'plays'(
+	uid VARCHAR(128) NOT NULL,
+	gid INTEGER NOT NULL,
+	finalscore INTEGER NOT NULL, 
+	PRIMARY KEY (uid, gid),
+	FOREIGN KEY (uid) REFERENCES Users(uid),
+	FOREIGN KEY (gid) REFERENCES Game(gid)
+);
+
+CREATE TABLE 'Game'(
+	gid INTEGER AUTO_INCREMENT NOT NULL, 
+	PRIMARY KEY(gid)
+);
 
 INSERT INTO Users VALUES ("krisht", "Krishna Thiyagarajan", "test123"); 
 INSERT INTO Users VALUES ("rossk", "Ross Kaplan", "test123"); 
