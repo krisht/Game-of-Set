3fd7755d2940cbebfc6c447f761493101b32a284
Krishna Thiyagarajan <krisht@cooper.edu>
2017-01-21 21:56:07 -0500

    Added more sql stuff

--- schema.sql
+++ schema.sql
@@ -7,27 +7,29 @@ GRANT ALL PRIVILEGES ON ReadySetGo.* to admin@localhost IDENTIFIED BY 'admin';
 USE ReadySetGo; 
 
 CREATE TABLE 'Users'(
-	uid VARCHAR(128) NOT NULL,
+	uid VARCHAR(128),
 	name VARCHAR(128), 
 	password VARCHAR(64),
+	joindate DATETIME DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(username)
-); 
+);
 
-CREATE TABLE 'plays'(
-	uid VARCHAR(128) NOT NULL,
-	gid INTEGER NOT NULL,
-	finalscore INTEGER NOT NULL, 
+CREATE TABLE 'playsin'(
+	uid VARCHAR(128),
+	gid INTEGER,
+	finalscore INTEGER DEFAULT 0,
 	PRIMARY KEY (uid, gid),
 	FOREIGN KEY (uid) REFERENCES Users(uid),
 	FOREIGN KEY (gid) REFERENCES Game(gid)
 );
 
 CREATE TABLE 'Game'(
-	gid INTEGER AUTO_INCREMENT NOT NULL, 
+	gid INTEGER AUTO_INCREMENT,
+	gametime DATETIME DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(gid)
 );
 
-INSERT INTO Users VALUES ("krisht", "Krishna Thiyagarajan", "test123"); 
-INSERT INTO Users VALUES ("rossk", "Ross Kaplan", "test123"); 
-INSERT INTO Users VALUES ("abhinavj", "Abhinav Jain", "test123"); 
-INSERT INTO Users VALUES ("brendas", "Brenda So", "test123"); 
+INSERT INTO Users(uid, name, password) VALUES ("krisht", "Krishna Thiyagarajan", "test123"); 
+INSERT INTO Users(uid, name, password) VALUES ("rossk", "Ross Kaplan", "test123"); 
+INSERT INTO Users(uid, name, password) VALUES ("abhinavj", "Abhinav Jain", "test123"); 
+INSERT INTO Users(uid, name, password) VALUES ("brendas", "Brenda So", "test123"); 
