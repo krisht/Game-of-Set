2092dc77ab6132b4d20100621497fb5415f2287e
Krishna Thiyagarajan <krisht@cooper.edu>
2017-01-23 00:51:33 -0500

    Added prototype classes. Will develop them more later.

---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Runner {
+
+
+    private ArrayList<User> activeUsers = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
+
+
---
+++ src/User.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+public class User {
+
+    private String uid;
+    private String pass;
+    private String name;
+    private Date joinDate;
+
+    public User(String uid, String pass, String name) {
+        this.uid = uid;
+        this.pass = pass;
+        this.name = name;
+        this.joinDate = new Date();
+    }
+
+    public String getUid() {
+        return this.uid;
+    }
+
+    public String getPass() { //maybe protected or private?
+        return this.pass;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public String toString() {
+        return "UID: " + uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setJoinDate(Date date) {
+        this.joinDate = date;
+    }
+
+
+    public boolean pushToDB() {
+        //push this user's information to database
+        return true;
+    }
+
+}
