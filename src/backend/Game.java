a8337ee4cccdc6faf5fda0a9c9ffe15cbe5a18c6
Krishna Thiyagarajan <krisht@cooper.edu>
2017-01-23 14:43:38 -0500

    Added more classes

---
+++ src/backend/Game.java
@@ -0,0 +1,40 @@
+
+package backend;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<User> users;
+    private int [] currScores;
+
+    public Game(ArrayList<User> users){
+        this.users = users;
+        currScores = new int[this.users.size()];
+        // insert time somehow
+    }
+
+    public Game(int numPlayers){
+        this.users = new ArrayList<>();
+        currScores = new int[numPlayers];
+    }
+
+    public ArrayList<User> getUsers(){
+        return this.users;
+    }
+
+    public int[] getCurrScores(){
+        return this.currScores;
+    }
+
+    public void setCurrScores(int[] scores){
+        this.currScores = scores;
+    }
+
+    public void setUsers(ArrayList<User> users){
+        this.users = users;
+    }
+
+
+
+}
---
+++ src/backend/Info.java
@@ -0,0 +1,28 @@
+package backend;
+
+
+import  java.sql.*;
+
+public class Info {
+
+    public int insertUser(String uid, String name, String pass) throws Exception{
+        //do stuff
+        return 0;
+    }
+
+    public int getUser(String uid, String pass) throws Exception{
+        // do stuff
+        return 0;
+    }
+
+    public int getGame(int gid){
+        //do stuff
+        return 0;
+    }
+
+    public int insertGame(){
+        //do stuff
+        return 0;
+    }
+
+}
