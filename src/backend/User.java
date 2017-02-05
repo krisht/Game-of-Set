7562ccc5377a24ee4bf4e15211fe4d7645b9f42f
rosskaplan <ross.kaplan96@gmail.com>
2017-02-05 15:15:16 -0500

    Password match will happen on frontend

--- src/backend/User.java
+++ src/backend/User.java
@@ -58,25 +58,12 @@ public class User {
         return true;
     }
 
-    public int userLogin() {
-        //call DB
-        //SELECT * from Users where Users.name = 'username' AND Users.password = 'password';
-        //Return 0 on success, -1 on failure
-    }
-
     public int userLogin(String username, String password) {
         //call DB
         //SELECT * from Users where Users.name = 'username';
         //If no match, return 2
         //SELECT * from Users where Users.name = 'username' AND Users.password = 'password';
         //Return 0 on success, 1 on failure (invalid password)
-    }
-
-    public boolean pwdMatch(String pass, String repeatpass) {
-        //no call to DB
-        if (pass.equals(repeatpass))
-            return true;
-        return false;
     }
 
     public boolean createUser(String username, String name, String password) {
