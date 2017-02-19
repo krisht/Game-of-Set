50856fc862323df0aa221ef2b98c7c6ba6deffe4
rosskaplan <ross.kaplan96@gmail.com>
2017-02-18 21:10:50 -0500

    Database connection DOES NOT WORK. Looking into alternatives.

--- src/backend/User.java
+++ src/backend/User.java
@@ -1,8 +1,13 @@
-package backend;
-
 import java.util.Date;
+import java.sql.*;
+import java.math.*;
 
 public class User {
+
+    public static void main(String args[]) {
+        int connValue = connectToDB();
+        System.out.println(connValue);
+    }
 
     private String uid;
     private String pass;
@@ -58,12 +63,33 @@ public class User {
         return true;
     }
 
+    public static int connectToDB() {
+        try {
+            String URL = "jdbc:mysql://199.98.20.115:5122/ReadySetGo";
+            String USER = "root";
+            String PASS = "brenda2";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Database connection established!");
+        }
+        catch(Exception ex) {
+           System.out.println("Error: unable to connect to database!");
+           System.exit(1);
+        }
+
+        //instantiate new jdbc connection as private member up top^^
+        //then if connection already exists, return working.
+        //otherwise, connect. if fails, return failed!
+        return 0;
+    }
+
     public int userLogin(String username, String password) {
         //call DB
         //SELECT * from Users where Users.name = 'username';
         //If no match, return 2 (user DNE), -1 on DB failure
         //SELECT * from Users where Users.name = 'username' AND Users.password = 'password';
         //Return 0 on success, 1 on failure (invalid password)
+        return 0;
     }
 
     public int createUser(String username, String name, String password) {
@@ -72,5 +98,6 @@ public class User {
         //Return 1 on successful match (User already exists), -1 on DB failure
         //INSERT INTO Users (username, name, password) VALUES (username, name, password);
         //Return 0 on successful insertion, return -1 on DB failure
+        return 0;
     }
 }
