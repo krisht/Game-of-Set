1cfa85a37ab350684d50ac73b74335e08522e1aa
Abhinav Jain <jain@cooper.edu>
2017-02-20 14:49:34 -0500

    added missing bracket

--- src/frontend/LoginPage.java
+++ src/frontend/LoginPage.java
@@ -115,7 +115,7 @@ public class LoginPage extends JFrame implements ActionListener, ItemListener {
 			// Check that both fields are present
 
 			if ((value1 != null && !value1.isEmpty()) && (value2 != null && !value2.isEmpty())) {
-				switch(Login(value1, value2){
+				switch(Login(value1, value2)){
 					case USER_NOT_EXIST:
 						System.out.println("ERROR: User does not exist.");
 						JOptionPane.showMessageDialog(this, "User does not exist in database", "Error", JOptionPane.ERROR_MESSAGE);
