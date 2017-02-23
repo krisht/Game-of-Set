077795cc82e04a3f6db50df6c2391a53b0d3700a
Krishna Thiyagarajan <krisht@cooper.edu>
2017-02-23 01:01:24 -0500

    Haven't tested changes yet but implemented

--- src/backend/GameBoard.java
+++ src/backend/GameBoard.java
@@ -10,26 +10,93 @@ public class GameBoard {
 
     private ArrayList<Integer> board = new ArrayList<>();
     private ArrayList<Integer> deck = new ArrayList<>();
+    private boolean initialized = false;
 
     public JSONObject initialize() {
-        generateCards();
-        addCards(12);
+        if (!initialized) {
+            int added = 0;
+            while (added != 12) {
+                board.clear();
+                deck.clear();
+                generateCards();
+                added = addCards(12);
+            }
+            initialized = true;
+        }
         return sendToFE();
+    }
+
+    public JSONObject requestCards() {
+        int added = addCards(3);
+        return sendToFE().put("numAdded", added);
     }
 
     public JSONObject processSubmission(int c1, int c2, int c3) { //maybe add json compatibility here instead of using ints
         if ((board.contains(c1) && board.contains(c2) && board.contains(c3)) && checkSet(c1, c2, c3)) {
-            removeSet(c1, c2, c3);
-            if (board.size() < 12 && deck.size() > 0)
-                addCards(3);
-            return sendToFE().append("correctSet", true); // Everything's guch
+            JSONObject obj1 = updateBoard(c1, c2, c3);
+            JSONObject obj2 = sendToFE();
+
+            for (String key : obj1.keySet())
+                obj2.put(key, obj1.get(key));
+
+            return obj2.put("setCorrect", true);
         }
-        return sendToFE().append("correctSet", false);
+        return sendToFE().put("setCorrect", false);
     }
 
-    private void addCards(int numDeal) {
+
+    private JSONObject updateBoard(int c1, int c2, int c3) {
+        int tmp1 = board.indexOf(new Integer(c1));
+        int tmp2 = board.indexOf(new Integer(c2));
+        int tmp3 = board.indexOf(new Integer(c3));
+        JSONObject tmpObj = new JSONObject();
+
+        if (board.size() < 12 && deck.size() > 0) {
+            if (deck.size() >= 3) {
+                board.set(tmp1, deck.get(0));
+                board.set(tmp2, deck.get(0));
+                board.set(tmp3, deck.get(0));
+                int[] replaced = {tmp1, tmp2, tmp3};
+                tmpObj.put("replaced", replaced);
+                return tmpObj;
+            }
+
+            if (deck.size() == 2) {
+                board.set(tmp1, deck.get(0));
+                board.set(tmp2, deck.get(0));
+                board.set(tmp3, -1);
+                int[] replaced = {tmp1, tmp2};
+                tmpObj.put("replaced", replaced);
+                return tmpObj;
+            }
+
+            if (deck.size() == 1) {
+                board.set(tmp1, deck.get(0));
+                board.set(tmp2, -1);
+                board.set(tmp3, -1);
+                int[] replaced = {tmp1};
+                tmpObj.put("replaced", replaced);
+                return tmpObj;
+            }
+        }
+
+        int[] replaced = {};
+        board.set(tmp1, -1);
+        board.set(tmp2, -1);
+        board.set(tmp3, -1);
+        tmpObj.put("posReplaced", replaced);
+        return tmpObj;
+    }
+
+
+    private int addCards(int numDeal) {
+        int numAdded = 0;
         for (int ii = 0; ii < numDeal; ii++)
-            board.add(deck.remove(0));
+            if (deck.size() >= 1) {
+                board.add(deck.remove(0));
+                numAdded++;
+            }
+        return numAdded;
     }
 
     private void generateCards() {
