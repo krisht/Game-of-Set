b930621f8521ea12d618334d28fc6df40ec75950
Server <server@server>
2017-02-27 22:36:20 -0500

    Setup backend server, moving to client vim

--- src/comms/ServerSide.java
+++ src/comms/ServerSide.java
@@ -1,5 +1,22 @@
-package comms;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class commServer {
+    
+    public static void main (String [] args) {
+    
+        int portNumber = 5000; //just any arbitrary value 
+        try {
 
-public class ServerSide {
-}
+            while(true) {
+                Socket socket = listener.accept();
+                try {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new 
+            
+
+    }
