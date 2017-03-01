8119d426c9b29fcefcb1c3ceb8afc62d2356eedc
Server <server@server>
2017-03-01 15:59:08 -0500

    Fixed compiler errors on ServerConn

---
+++ src/comms/ServerConn.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerConn {
+
+    private static ServerSocket listener;
+    private static Socket socket;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Socket attempt");
+        try {
+            String temp;
+            BufferedReader in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            while ((temp = in.readLine()) != null) {
+                out.println("we saw " + temp);
+            }
+        } catch (Exception e) {
+            System.out.println("nope boi");
+        } finally {
+            try {
+                listener.close();
+            } catch(Exception e) {
+                System.out.println("ehhhhh not working");
+            } 
+        }
+    }
+
+    public static void ServerConn() throws Exception {
+        ServerConn serverConn = new ServerConn();
+        listener = new ServerSocket(5000);
+        socket = listener.accept();
+    }
+        
+}
