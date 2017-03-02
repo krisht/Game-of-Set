838b728fec359cb3dceeaea2821fdf01681e0352
Krishna Thiyagarajan <krisht@cooper.edu>
2017-03-01 19:01:13 -0500

    Started adding JSON compatibility to the ClientConn

--- src/comms/ClientConn.java
+++ src/comms/ClientConn.java
@@ -1,41 +1,64 @@
+package comms;
+
+import org.json.JSONObject;
+
+import javax.swing.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ClientConn {
 
+    private static Socket socket;
+    private static int uid;
     private BufferedReader in;
     private PrintWriter out;
-    private Socket socket;
+    private int gid;
 
-    public static void main(String[] args) throws Exception {
-        ClientConn clientConn = new ClientConn();
-        clientConn.connectToServer();
-        clientConn.sendMessages();
-    }
+    public ClientConn(int uid) {
+        try {
+            socket = new Socket("199.98.20.115", 5000);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
 
-    public void connectToServer() throws Exception {
-        System.err.println("frst");
-        socket = new Socket("199.98.20.115", 5000);
+            System.err.println("Connected to host successfully!");
 
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
-        System.out.println("second");
+        } catch (IOException exc) {
+            JOptionPane.showMessageDialog(new JFrame(),
+                    "Error connecting to server!",
+                    "Server Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            System.exit(-1);
+        }
 
-        System.err.println(in.readLine());
-    }
+        JSONObject firstMessage = new JSONObject();
+        firstMessage.append("uid", uid);
 
-    public void sendMessages() throws Exception {
 
-        Scanner scan = new Scanner(System.in);
-        String input;
-
-        while ((input = scan.nextLine()) != null) {
-            this.out.println(input);
-            System.out.println(in.readLine());
+        try {
+            messageServer(firstMessage);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(new JFrame(),
+                    "Unable to communicate with connected server!",
+                    "Communication Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            System.exit(-1);
         }
 
     }
+
+    public static void main(String[] args) throws Exception {
+        ClientConn clientConn = new ClientConn(1);
+    }
+
+    public JSONObject messageServer(JSONObject obj) throws Exception {
+        String request = obj.toString();
+        this.out.println(request);
+        return new JSONObject(in.readLine());
+    }
+
 }
--- src/comms/ServerThread.java
+++ src/comms/ServerThread.java
@@ -1,3 +1,5 @@
+package comms;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
