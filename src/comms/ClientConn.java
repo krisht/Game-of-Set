71661f4c0b733882106b39d23bb7667bafc05142
Krishna <krisht@cooper.edu>
2017-03-15 14:05:18 -0400

    Fixed some SQL stuff

--- src/comms/ClientConn.java
+++ src/comms/ClientConn.java
@@ -1,6 +1,7 @@
+package comms;
+
 import org.json.JSONObject;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/comms/ServerThread.java
+++ src/comms/ServerThread.java
@@ -1,3 +1,5 @@
+package comms;
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -14,48 +16,45 @@ public class ServerThread extends Thread {
 
     private BufferedReader in;
     private PrintWriter out;
-    private String received_data;
     private String return_data;
 
     public ServerThread(Socket sock) {
         Socket tempsock = sock;
+        JSONObject obj = new JSONObject();
 
         try {
             in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             out = new PrintWriter(sock.getOutputStream(), true);
-
-            received_data = in.readLine();
-            JSONObject obj = new JSONObject(received_data);
-
-            //System.err.println("RECEIVED: " + received_data);
-
+            obj = new JSONObject(in.readLine());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
         this.sock = sock;
-        this.processData(); //Uses received_data, figures out which BE function to call
-                            //And fills in return_data
+        this.processData(obj); //Uses received_data, figures out which BE function to call
+        //And fills in return_data
 
         this.out.println(return_data);
-        
+
     }
 
-    public void processData() {
+    public void processData(JSONObject obj) {
+
+        if (obj.length() == 0)
+            return_data = obj.toString();
+
         try {
-            //System.err.println("RECEIVED: " + received_data);
-            JSONObject received_object = new JSONObject(received_data);
-            String temp = received_object.getJSONArray("fCall").toString().substring(2);
+            String temp = obj.getJSONArray("fCall").toString().substring(2);
             temp = temp.substring(0, temp.length()-2);
             switch(temp) {
-                case "CheckSet":
+                case "checkSet":
                     //System.err.println("In Checkset");
                     //CALL CHECKSET, RETURN IT INTO private STRING "return_data" HERE 
                     break;
                 case "Other":
                     //System.err.println("In Other");
                     break;
-                default: 
+                default:
                     //error
                     break;
             }
