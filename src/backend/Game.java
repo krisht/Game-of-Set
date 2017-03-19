e4e5e00b998a2655f9d0eb168d5e0cfd27861c53
Krishna <krisht@cooper.edu>
2017-03-18 22:14:19 -0400

    Working on backend.Game.java. Changed DBComm so that DBInsert returns inserted key. Added GameListing for logging into lobby and whatnot.

--- src/backend/Game.java
+++ src/backend/Game.java
@@ -4,6 +4,9 @@ package backend;
 import org.json.JSONObject;
 
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Game {
@@ -14,34 +17,47 @@ public class Game {
     private GameBoard board = new GameBoard();
     private HashMap<Integer, User> listOfPlayers = new HashMap<>();
 
-    public Game() throws Exception {
-
+    public Game(String gameName) {
         try {
-            gameDB.DBInsert("INSERT INTO TABLE Game() VALUES ()");
+            gameDB.DBInsert("INSERT INTO Game(gname) VALUES(" + gameName + ")");
             ResultSet set = gameDB.DBQuery("SELECT * FROM GAME");
-            if (set.next())
-                gid = set.getInt(1);
+            if (set.next()) {
+                this.gid = set.getInt(1);
+                this.gameName = gameName;
+            } else throw new Exception("Game not inserted into database!");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        board.initialize();
+    }
+
+    public Game() throws Exception {
+        DateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        String gName = "game" + dateFormat.format(date);
+        try {
+            gameDB.DBInsert("INSERT INTO Game(gname) VALUES(" + gName + ")");
+            ResultSet set = gameDB.DBQuery("SELECT * FROM GAME");
+            if (set.next()) {
+                this.gid = set.getInt(1);
+                this.gameName = gName;
+            }
             else throw new Exception("Game not inserted into database!");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-        gameName = "game" + gid;
-
         board.initialize();
     }
 
-    public Game(String gameName) throws Exception {
-        this();
-        this.gameName = gameName;
-    }
-
     public JSONObject addToGame(int uid) {
+        User
 
     }
 
     public JSONObject removeFromGame(int uid) {
-
+        listOfPlayers.remove(uid);
+        //Return JSONObject indicating player remove properly
     }
 
     public JSONObject getScoreOfPlayer(int uid) {
