e920f09ba58882062c936f886c802177053fbdac
Krishna <krisht@cooper.edu>
2017-04-15 20:10:02 -0400

    Testbench for Backend

---
+++ src/backend/TestBench.java
@@ -0,0 +1,10 @@
+package backend;
+
+public class TestBench {
+
+    public static void main(String[] args) {
+        Game game = new Game(1);
+        System.out.println(game.getGameName());
+    }
+
+}
