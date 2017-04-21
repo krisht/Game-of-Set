1c624d6c6df6660ad439f8b907925df12dfd3713
Krishna <krisht@cooper.edu>
2017-04-21 18:26:24 -0400

    UpdatestoCommsProtocol

--- src/backend/ServerThread.java
+++ src/backend/ServerThread.java
@@ -210,7 +210,7 @@ public class ServerThread implements Runnable {
 
                         System.out.println("Tempobj: " + tempobj);
 
-                        temparr.add(tempobj);
+                        temparr.put(tempobj);
                         ///blablahblah
                         System.out.println("Temparr: " + temparr);
                     }
