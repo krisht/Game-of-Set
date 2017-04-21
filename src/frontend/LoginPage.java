647fae18c6b3505d7f1b31ef1bc21b56af7b6976
Abhinav <jain@cooper.edu>
2017-04-21 00:08:44 -0400

    silly updates

--- src/frontend/LoginPage.java
+++ src/frontend/LoginPage.java
@@ -404,7 +404,7 @@ public class LoginPage extends JFrame implements ActionListener{
         try {
 
             // Create a landing page
-            LandingPage_New landingpage = new LandingPage_New();
+            LandingPage landingpage = new LandingPage();
 
             // NOTE: The proper way as implemented in the landing page closes the landing page too, so use this way
             this.setVisible(false);
