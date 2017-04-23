bab2a12b96ed2f83b1d3ceeb93d0965b0b004cbe
rosskaplan <ross.kaplan96@gmail.com>
2017-04-23 17:23:30 -0400

    fifth fixing of no more sets

--- src/backend/ServerThread.java
+++ src/backend/ServerThread.java
@@ -134,9 +134,6 @@ class ServerThread implements Runnable {
                     Game game = GameListing.getGame(gid);
                     String username = game.getPlayerList().get(uid).getUsername();
                     tempobj = GameListing.getGame(gid).userSubmits(uid, c1, c2, c3).put("fCall", "userSubmitsResponse").put("username", username);
-                    if (tempobj.get("returnValue") == 1) {
-                        GameListing.getGame(gid).getGameBoard().updateBoard(c1, c2, c3);
-                    }
                     Map<Integer, Game> games = GameListing.getGames();
                     ArrayList<User> users = new ArrayList<>(games.get(gid).getPlayerList().values());
                     ArrayList<Integer> uids = new ArrayList<>();
