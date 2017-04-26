b48354762568c9021215b6cc5341d8e2032b4687
Krishna <krisht@cooper.edu>
2017-04-26 13:50:30 -0400

    Shit

--- src/backend/DBComm.java
+++ src/backend/DBComm.java
@@ -16,7 +16,7 @@ class DBComm {
         }
         try {
             //bs
-            conn = DriverManager.getConnection("jdbc:mysql://199.98.20.115/ReadySetGo?user=ross&password=ross2&verifyServerCertificate=false&useSSL=true&autoReconnect=true");
+            conn = DriverManager.getConnection("jdbc:mysql://199.98.20.122/ReadySetGo?user=ross&password=ross2&verifyServerCertificate=false&useSSL=true&autoReconnect=true");
             System.err.println("Database connection established!");
         } catch (Exception ex) {
             ex.printStackTrace();
