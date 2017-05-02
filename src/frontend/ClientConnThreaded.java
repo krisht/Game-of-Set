0378d7459170d5ea985e08230993cb00d8cfc64b
Krishna <krisht@cooper.edu>
2017-05-02 15:22:15 -0400

    Changed how images are loaded

--- src/frontend/ClientConnThreaded.java
+++ src/frontend/ClientConnThreaded.java
@@ -1,5 +1,6 @@
 package frontend;
 
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -7,10 +8,7 @@ import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
 import javax.swing.text.StyledDocument;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -152,6 +150,7 @@ public class ClientConnThreaded extends JFrame implements Runnable {
                                                 doc.insertString(doc.getLength(), "System: ", gameSystemStyle);
                                                 doc.insertString(doc.getLength(), "You scored!", GameStyle);
                                                 doc.insertString(doc.getLength(), "\n", GameStyle);
+
                                             }
                                             catch (BadLocationException e) {
                                                 e.printStackTrace();
